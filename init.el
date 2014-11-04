bacfccce236ceeaa1b77163d716f039862ce546c
nosami <jasonimison@gmail.com>
2014-11-04 00:35:41 +0000

    better company mode

--- init.el
+++ init.el
@@ -18,3 +18,27 @@
 
 (tool-bar-mode -1)
 (load-theme 'monokai t)
+(setq company-begin-commands '(self-insert-command))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-frontends
+   (quote
+    (company-pseudo-tooltip-frontend company-echo-metadata-frontend)))
+ '(company-idle-delay 0.03)
+ '(company-minimum-prefix-length 1)
+ '(company-show-numbers t)
+ '(helm-ag-insert-at-point (quote word))
+ '(omnisharp-auto-complete-want-documentation nil)
+ '(omnisharp-company-sort-results t)
+ '(omnisharp-server-executable-path
+   (quote /Users/jason/\.vim/bundle/Omnisharp/server/OmniSharp/bin/Debug/OmniSharp\.exe))
+ '(safe-local-variable-values
+   (quote
+    ((eval when
+           (fboundp
+            (quote rainbow-mode))
+           (rainbow-mode 1)))))
+ '(savehist-mode t)) 
