948bffa8a1161035d7206179ddb6d81664027462
nosami <jasonimison@gmail.com>
2014-11-04 19:26:34 +0000

    only change font to consolas when on windows

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(defvar mswindows-p (string-match "windows" (symbol-name system-type)))
 (require 'package)
 (setq package-archives
       '(("melpa"
@@ -59,13 +60,16 @@
  '(savehist-mode t)
  '(show-paren-mode t)
  '(tool-bar-mode nil))
+(when mswindows-p
+  (set-face-attribute 'default nil
+		      :family "Consolas" :height 100))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
+ 
  '(company-tooltip-annotation ((t (:inherit company-tooltip :foreground "yellow")))))
 (global-hl-line-mode 1)
 (setq c-basic-offset 4) ; indents 4 chars
@@ -94,3 +98,5 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
 (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
+
