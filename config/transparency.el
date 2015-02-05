70a062a2145c7ec460eb9a9eaaa03614e3210f53
nosami <jasonimison@gmail.com>
2015-02-05 23:35:54 +0000

    removed transparency

--- config/transparency.el
+++
@@ -1,4 +0,0 @@
-;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
-(set-background-color "#000")
-(set-frame-parameter (selected-frame) 'alpha '(80 70))
-(add-to-list 'default-frame-alist '(alpha 80 70))
